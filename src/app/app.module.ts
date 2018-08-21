0cc1ff9bbe93006048063845677abd5cf8d0c67d
Rusakov <rusakov.nikola+programming@gmail.com>
2018-08-21 15:33:40 +0200

    init commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BaseLayoutComponent } from './components/base-layout/base-layout.component';
+import { SidebarPanelComponent } from './components/sidebar-panel/sidebar-panel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BaseLayoutComponent,
+    SidebarPanelComponent
   ],
   imports: [
     BrowserModule
